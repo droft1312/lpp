3f7fa3868325eec098ece097cea5aa67fff3a943
Turar Jumaniyaz <droft1312@gmail.com>
2019-05-26 12:06:00 +0200

    created a NAND node

---
+++ LPP/Nodes/NandNode.cs
@@ -0,0 +1,25 @@
+namespace LPP.Nodes
+{
+    public class NandNode : Node
+    {
+        public NandNode (string input, Node parent) : base (input, parent) {
+
+        }
+        
+        public override string GetInfix () {
+            string res = "(" + left.GetInfix () + ")" + " % " + "(" + right.GetInfix () + ")";
+            return res;
+        }
+        
+        public override bool GetValue (string input) {
+            var leftVal = left.GetValue (input);
+            var rightVal = right.GetValue (input);
+
+            return !(leftVal && rightVal);
+        }
+        
+        public override string ToString () {
+            return "%";
+        }
+    }
+}
